4b3f091c44bf065dfffb0a75a6fa919df35484ad
Dev <khoanguyenanh@hotmail.com>
2023-10-15 00:01:09 +0700

    create bew

---
+++ chitmeo/chitmeo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chitmeo
+//
+//  Created by dev on 15/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chitmeo/chitmeo/chitmeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chitmeoApp.swift
+//  chitmeo
+//
+//  Created by dev on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct chitmeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
