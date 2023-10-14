6da31e0b90912fa8436a0a354720c1f3b56aea47
Dev <khoanguyenanh@hotmail.com>
2023-10-15 00:23:53 +0700

    create some view and group folder

---
+++ chitmeo/chitmeo/Screens/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  chitmeo
+//
+//  Created by dev on 15/10/2023.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
---
+++ chitmeo/chitmeo/Views/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  chitmeo
+//
+//  Created by dev on 15/10/2023.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
